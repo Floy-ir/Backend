7efbc131a626a5024375ac87cd67c77688da44c8
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-03-19 14:13:01 +0330

    feat: add health check to project

--- apps/file_storage/interfaces/abstractions.py
+++ apps/file_storage/interfaces/abstractions.py
@@ -1,6 +1,6 @@
 import abc
 from apps.accounts import interfaces as accounts_interfaces
-from .dataclasses import UploadMetadata, UploadRequest, UploadMetadataFilter, UploadMetadataResult, ImagesLink
+from .dataclasses import UploadMetadata, UploadRequest, ImagesLink
 
 
 class AbstractFileStorageService(abc.ABC):
@@ -11,7 +11,7 @@ class AbstractFileStorageService(abc.ABC):
 
         Args:
             caller (Session): the caller. for now, it can be only apps
-            upload_request (UploadRequest): including files
+            request (UploadRequest): including files
 
         Returns:
             UploadMetadata: contains metadata about the file upload
