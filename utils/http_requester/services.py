d78727ec0bf64355d061bdbb21b8c680b8627d96
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-03-29 16:08:15 +0330

    feat: add url as parameter to request in request module

--- utils/http_requester/services.py
+++ utils/http_requester/services.py
@@ -1,5 +1,4 @@
 from typing import List, Tuple
-from urllib.parse import urljoin
 import requests
 import logging
 from . import interfaces
@@ -9,46 +8,38 @@ logger = logging.getLogger(__name__)
 
 class RequestsHTTPRequester(interfaces.AbstractHTTPRequester):
 
-    def request(self, method: str, base_addresses: List[str], path: str, data=None, retry_statuses: List[int] = None,
-                parse_response_as_json: bool = True, timeout: Tuple[int, int] = (10, 301),
+    def request(self, method: str, url: str, data=None, retry_statuses: List[int] = None,
+                parse_response_as_json: bool = True, timeout: Tuple[int, int] = (10, 30),
                 **kwargs) -> interfaces.RequesterResponse:
-        logger.info(f"method:{method},base_addresses:{base_addresses},path:{path},data:{data},"
-                    f"retry_statuses:{retry_statuses},parse_response_as_json:{parse_response_as_json},"
-                    f"timeout:{timeout},kwargs:{kwargs}")
+        logger.info(f"method:{method},url:{url},data:{data},retry_statuses:{retry_statuses},"
+                    f"parse_response_as_json:{parse_response_as_json},timeout:{timeout},kwargs:{kwargs}")
+
         if retry_statuses is None:
             retry_statuses = [500, 502, 503, 504]
-        for base_address in base_addresses:
-            the_url = urljoin(base_address, path)
-            logger.debug(f"the url is: url: {the_url}")
 
-            try:
-                response = requests.request(
-                    method=method,
-                    url=the_url,
-                    data=data,
-                    json=kwargs.get("json", None),
-                    timeout=timeout,
-                    params=kwargs.get("params", None),
-                    headers=kwargs.get("headers", None),
-                )
-            except requests.exceptions.ConnectionError as e:
-                logger.warning(
-                    'Connection error occurred while requesting URL: %s. Error details: %s',
-                    the_url,
-                    str(e)
-                )
-                raise interfaces.ConnectionErrorException(f'Connection error occurred. URL: {the_url}')
+        logger.debug(f"Requesting URL: {url}")
 
-            except requests.exceptions.Timeout as e:
-                logger.warning(f'Timeout occurred while requesting {the_url}: {e}')
-                raise interfaces.TimeOutException(f'Request timed out {the_url}')
+        try:
+            response = requests.request(
+                method=method,
+                url=url,
+                data=data,
+                json=kwargs.get("json", None),
+                timeout=timeout,
+                params=kwargs.get("params", None),
+                headers=kwargs.get("headers", None),
+            )
+        except requests.exceptions.ConnectionError as e:
+            logger.warning(f'Connection error occurred while requesting URL: {url}. Error details: {e}')
+            raise interfaces.ConnectionErrorException(f'Connection error occurred. URL: {url}')
+        except requests.exceptions.Timeout as e:
+            logger.warning(f'Timeout occurred while requesting {url}: {e}')
+            raise interfaces.TimeOutException(f'Request timed out {url}')
 
-            if response.status_code not in retry_statuses:
-                break
-        else:
+        if response.status_code in retry_statuses:
             raise interfaces.RequestException(
-                status_code=500,
-                message="None of base addresses returned a non to-retry response"
+                status_code=response.status_code,
+                message="Response returned with a retryable status code"
             )
 
         content_json = None
@@ -57,6 +48,7 @@ class RequestsHTTPRequester(interfaces.AbstractHTTPRequester):
                 content_json = response.json()
             except requests.exceptions.JSONDecodeError as e:
                 logger.debug(e)
+
         result = interfaces.RequesterResponse(
             status_code=response.status_code,
             content_bytes=response.content,
