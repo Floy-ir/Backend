4a263eedde9c73009a2be0de19d37ee6b9e1d2fc
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-03-29 15:16:04 +0330

    feat: add website managing methods to crawler

---
+++ apps/flight_crawler/interfaces/exceptions.py
@@ -0,0 +1,26 @@
+from libs import exceptions as lib_exceptions
+
+
+class BadRequest(lib_exceptions.BadRequestRoot):
+    pass
+
+
+class Forbidden(lib_exceptions.ForbiddenRoot):
+    pass
+
+
+class NotFound(lib_exceptions.NotFoundRoot):
+    pass
+
+class ServiceUnavailable(lib_exceptions.ServiceUnavailableRoot):
+    pass
+
+
+class WebsiteNotFound(NotFound):
+    def __init__(self):
+        super().__init__("website doesn't exist")
+
+
+class FileStorageNotAvailable(ServiceUnavailable):
+    def __init__(self):
+        super().__init__("file storage is not available")
