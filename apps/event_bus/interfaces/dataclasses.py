f8ce86bb08eee04edf8908cef098bfbde905c690
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-04-11 15:40:21 +0330

    feat: add migrations of all apps

--- apps/event_bus/interfaces/dataclasses.py
+++ apps/event_bus/interfaces/dataclasses.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from pydantic import BaseModel
-import lib.data_classes as lib_data_classes
+import libs.dataclasses as lib_data_classes
 
 
 class EmissionType(str, Enum):
@@ -12,6 +12,6 @@ class EmissionType(str, Enum):
 
 class EventOrCommand(BaseModel):
     uid: str  # for duplicate emit prevention
-    event_type: lib_data_classes.CustomStringValidator.max_length_string(128)  # usually upper case
+    event_type: str
     payload: BaseModel  # each event has its own payload. the emitter should document this.
     emission_type: EmissionType = EmissionType.EVENT_OR_COMMAND
