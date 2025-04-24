7c3d74c89b9d3f65c3e85c198709b966443eb423
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-04-24 18:16:21 +0330

    fix: get flights method

--- runner/bootstrap.py
+++ runner/bootstrap.py
@@ -42,9 +42,9 @@ class Bootstrapper:
         _minio_bucket_name = os.getenv('MINIO_BUCKET_NAME', 'floy-bucket')
 
         # redis
-        _REDIS_HOST = "localhost"
-        _REDIS_PORT = 6379
-        _REDIS_DB = 0
+        _REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+        _REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
+        _REDIS_DB = int(os.getenv('REDIS_DB', '0'))
 
         # variables
         _max_adults = 2
@@ -102,8 +102,8 @@ class Bootstrapper:
             )
         )
 
-        self._flight_crawler = kwargs.get(
-            'flight_crawler',
+        self._flight_crawler_service = kwargs.get(
+            'flight_crawler_service',
             FlightCrawlerService(
                 claim=accounts_interfaces.Session.for_internal_app(uid='flight_crawler_service'),
                 date_time_utils=_date_time_utils,
@@ -123,9 +123,8 @@ class Bootstrapper:
             FlightsService(
                 claim=accounts_interfaces.Session.for_internal_app(uid='airlines_service'),
                 airlines_service=self._airlines_service,
-                date_time_utils=_date_time_utils,
                 event_bus=self._event_bus,
-                flight_crawler_service=self._flight_crawler,
+                flight_crawler_service=self._flight_crawler_service,
                 cache_service=self._cache_service
             )
         )
@@ -147,7 +146,7 @@ class Bootstrapper:
         return self._flights_service
 
     def get_flight_crawler_service(self) -> FlightCrawlerService:
-        return self._flight_crawler
+        return self._flight_crawler_service
     
     def get_event_bus(self) -> EventBus: 
         return self._event_bus
