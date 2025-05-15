bfdba8b4ea1d478afccaa5cc5e1326be32af3943
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-05-15 08:24:33 +0330

    Fix: celery

---
+++ runner/schedule/tasks.py
@@ -0,0 +1,18 @@
+from .celery import app
+from runner.bootstrap import get_bootstrapper
+
+
+@app.task
+def crawl_three_days_ahead(): 
+    service = get_bootstrapper().get_flight_crawler_service()
+    service.crawl_scheduled_flights(from_days_ahead=0, to_days_ahead=3)
+
+@app.task
+def crawl_four_and_more_days_ahead(): 
+    service = get_bootstrapper().get_flight_crawler_service()
+    service.crawl_scheduled_flights(from_days_ahead=3, to_days_ahead=11)
+
+@app.task()
+def test_celery(): 
+    service = get_bootstrapper().get_flight_crawler_service()
+    service.test_celery()
