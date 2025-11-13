3fce7847e556c6ca1ace03536e681511fbf0fc20
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-11-13 21:10:51 +0330

    fix

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -39,7 +39,7 @@ class OTP(models.Model):
 
 class EitaUser(models.Model):
     uid = models.CharField(max_length=128, unique=True)
-    eita_id = models.CharField(max_length=256, unique=True)
+    eita_id = models.CharField(max_length=256, unique=True, default="nothing")
     created_at = models.PositiveBigIntegerField(default=0)
     last_login_at = models.PositiveBigIntegerField(default=0)
 
